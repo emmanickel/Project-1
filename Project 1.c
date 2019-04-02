83f2e12fb6d82c54d1bafedbdb80f1e66f3c8268
asdf asdf <a@b.com>
2019-04-02 01:27:12 +0000

    Task selection start

--- Project 1.c
+++ Project 1.c
@@ -1,17 +1,43 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
+    int taskNumber;
+
+//User friendly task selection menu system
+    printf("Please select a task(1, 3, 4, 5, 6 or 7): \n"); // Asks user which task they would like performed
+    printf("(1) Encryption with a rotation cipher given plain text and key\n");
+    printf("(2) Decryption with a rotation cipher given cipher text and key\n");
+    printf("(3) Encryption with a substitution ciper given plain text and key\n");
+    printf("(4) Decryption with a substitution cipher given cipher text and key\n");
+    printf("(5) Decryption of a previously unseen cipher text encrypted with a rotation cipher\n");
+    printf("(6) Decryption of a day-1 provided block of cipher text encrypted with a substitution cipher\n");
+    printf("(7) Decryption of a day-1 provided block of cipher text encrypted with a substitution cipher\n");
+    printf("Selection: ");
+    
+    //scanf("%d", &taskNumber);  This reads the user's input. NB: scanf() doesn't work right now!
+    
+
+    
+
+
+
 
 /* Project Planning:
 
-Task 1: Encryption 
-asjhdjf
-Task 2:
+Task 1: Encryption with a rotation cipher given plain text and key
 
-Task 3:
+Task 2: Decryption with a rotation cipher given cipher text and key
 
-Task 4:
+Task 3: Encryption with a substitution ciper given plain text and key
+
+Task 4: Decryption with a substitution cipher given cipher text and key
+
+Task 5: Decryption of a previously unseen cipher text encrypted with a rotation cipher
+
+Task 6: Decryption of a day-1 provided block of cipher text encrypted with a substitution cipher
+
+Task 7: Decryption of an unseen block of cipher text on marking day
 
 
  *\
