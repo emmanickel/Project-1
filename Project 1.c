94a323b6e90cf2dd0a9b95d508aa2430e6f6fc52
Emma Nickel <emmajanenickel@gmail.com>
2019-04-14 11:20:40 +0000

    Task  1 code complete

--- Project 1.c
+++ Project 1.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
 
-char encryptRotate(char str, int index);
+//function prototypes:
+
+char menusystem(void);
+char rotnEncryption(char* message);
+
 int main()
 {
-   // int taskNumber;
+    menusystem();
+    
+//Task 1: Encryption with a rotation cipher given plain text and key
+    
+    char message[1024]; //the message entered by the user
+    
+    printf("Enter a message to encrypt (in capital letters):\n");//the console tells the user to input a message
+    scanf("%[^\n]", message);//the user inputs a message
+    
+    rotnEncryption(message);//the message is encrypted by calling the function rotnEncryption
+}
 
-//User friendly task selection menu system
+//Function definitions:
 
-    /*printf("Please select a task(1, 3, 4, 5, 6 or 7): \n"); // Asks user which task they would like performed
+    char menusystem(void)
+{
+    int taskNumber;
+    
+    printf("Please select a task (1, 3, 4, 5, 6 or 7): \n"); // Asks user which task they would like performed
     printf("(1) Encryption with a rotation cipher given plain text and key\n");
     printf("(2) Decryption with a rotation cipher given cipher text and key\n");
     printf("(3) Encryption with a substitution ciper given plain text and key\n");
@@ -17,135 +35,68 @@ int main()
     printf("(7) Decryption of a day-1 provided block of cipher text encrypted with a substitution cipher\n");
     printf("Selection: ");
     
-    //scanf("%d", &taskNumber);  This reads the user's input. NB: scanf() doesn't work right now!
+    scanf("%d", &taskNumber);  //This reads the user's input
     // Write switch statements to ues input to select task.
     
     switch(taskNumber)
     {
-        case 1: Task1(); break;
-        case 2: Task2(); break;
-        case 3: Task3(); break;
-        case 4: Task4(); break;
-        case 5: Task5(); break;
-        case 6: Task6(); break;
-        case 7: Task7(); break;
+        case 1: 
+            printf("You have selected task (1).");
+            //task1(); (somehow get task 1 to run)
+            break;
+        case 2:
+            printf("You have selected task (2).");
+            //task1(); (somehow get task 2 to run)
+            break;
+        case 3:
+            printf("You have selected task (3).");
+            //task1(); (somehow get task 3 to run)
+            break;
+        case 4:
+            printf("You have selected task (4).");
+            //task1(); (somehow get task 4 to run)
+            break;
+        case 5:
+            printf("You have selected task (5).");
+            //task1(); (somehow get task 5 to run)
+            break;
+        case 6:
+            printf("You have selected task (6).");
+            //task1(); (somehow get task 6 to run)
+            break;
+        case 7:
+            printf("You have selected task (7).");
+            //task1(); (somehow get task 7 to run)
+            break;
         default: printf("Unknown option %d\nPlease enter 1, 2, 3, 4, 5, 6 or 7\n", taskNumber);
-    }*/
-    
-
-
-/*Project Planning:
-
-Task 1: Encryption with a rotation cipher given plain text and key
-- Input message to encrypt
-- Input key
-- Encrypt text
-- Print encrypted text*/
-
-
-
-#include <stdio.h>
-
-      //char encryption(char str, char index);
-char shiftletters(char str[int k]);
-
-
-int main()
-{
-    
- char str[1024] = "hello";//hardcoded for now
- int k;
- 
-
-    printf("Please enter a message to encrypt:\n");
-    //scanf("%s\n", str); // Note: NO '&'' REQUIRED! The user's message is entered as a string
-    //Note: Need to use uppercase letters only, and all white space, punctuation and numerals need to be ignored.
-    
-    printf("%s\n", str); //hardcoded message for now
-    
-    printf("Please enter a key to shift the letters by: ");
-    //scanf("%d, &rotateKey");
-    
-    printf("The encrypted message is: ");
-    
- for() //just for the hradcoded hello message
-    printf("%c", shiftletters());
-    
-    
-}  
-    
-    
-
-
-char shiftletters(char str)
-{
-   
-   
-int temp; //temporary number where A=0, B=1.. etc
-int encryption; //the new number after shifting
-char newletter; // the corresponding ASCII letter (after adding 65 back) */
-int shift = 5;
-
-    for(i = 65; i < 91; i++)
-    {
-        
-//  printf("%c %d\n", i, i);
-
-    temp = i - 65;
-
- // printf("%d\n", temp);
-    
-  encryption = (temp + shift)%26;
-    
- //printf("%d\n", encryption);
-    
-    newletter = encryption + 65;
-    
-    
-    return newletter;
-    } 
-  }
-
-
-
-
-FILE *input;
-
-input = fopen("encryptedmessage.txt", "w");
-while()
-
-    printf("Please enter a message to encrypt:\n");
-    //scanf("%s\n", str); // Note: NO '&'' REQUIRED! The user's message is entered as a string
-    //Note: Need to use uppercase letters only, and all white space, punctuation and numerals need to be ignored.
-    
-    
-    printf("Please enter a key to shift the letters by: ");
-    //scanf("%d, &rotateKey");
-    
-    printf("Encrypted message is: "); 
-    printf("%s", encryptRotate(str));
-    
-}
-
-    char encryptRotate(char str, int index) //Index is the ASCII value of a letter a given time
-    {
-        
-        
-        int rotateKey = 5;  //This is the key of encryption - the number of letters by which the alphabet is rotated to
-                         //calculate the substitution (temporarily hardcoded to 5 at the moment without scanf)*/
-        char str[1024] = "Hello"; //This initialises the user's input as a string. Should it be a constant?? Temporarily hardcoded
-    
-        for(index = 65; (index < 100 && index != 0); index++) //the index increments between 
-        {
-            str[index] = (str[index] + rotateKey); // Encryption calculation
-            return str;
-        }
-    
     }
 
 
-
-
+char rotnEncryption(char* message)
+{
+    int rotnKey;//the key of encryption (the number of letters the unser decides to shift by)
+    int index = 0;
+    char encryptedletter; // the corresponding ASCII letter after rotation encryption calculation
+    
+    printf("Please enter a key (a number from 1 to 25) to shift the letters by: ");//the console asks the user to input a key
+    scanf("%d", &rotnKey);//the user inputs a key
+    
+    //for loop that takes the message input by the user and reads it one character at a time until the terminating character '\0' is reached:
+    for(message[index]; message[index] != '\0'; index ++)
+    {
+        if(message[index] == 32)//if the character entered is a space (ASCII number 32), it is not encrypted but still printed as a space
+        {
+            printf(" ");//print a space
+        }
+        else if(message[index] != 32)//this accounts for all other cases where the characters are not spaces
+        {
+            encryptedletter = ((message[index]-65) + rotnKey)%26 + 65;
+            //the corresponding ASCII value of each character in the user's message is subtracted by 65, shifted by the given key then 65 id added
+            printf("%c", encryptedletter);//each character of the message is printed after being encrypted
+        }
+    }   
+}    
+    
 
 /*
 Task 2: Decryption with a rotation cipher given cipher text and key
